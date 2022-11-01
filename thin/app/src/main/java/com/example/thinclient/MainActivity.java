b752ab8a83bdb6adcbbfd600da89050d7da2d629
Roger Iyengar <ri@rogeriyengar.com>
2022-11-01 17:58:09 -0400

    Run with all files

--- thin/app/src/main/java/com/example/thinclient/MainActivity.java
+++ thin/app/src/main/java/com/example/thinclient/MainActivity.java
@@ -4,17 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.google.protobuf.ByteString;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 import edu.cmu.cs.gabriel.client.comm.ServerComm;
@@ -23,6 +34,8 @@ import edu.cmu.cs.gabriel.protocol.Protos;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private volatile boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +53,91 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        Consumer<Protos.ResultWrapper> consumer = resultWrapper -> {
-            Protos.ResultWrapper.Result result = resultWrapper.getResults(0);
-            ByteString byteString = result.getPayload();
-            byte[] bytes = byteString.toByteArray();
-            String className = new String(bytes);
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        running = true;
+        pool.execute(() -> {
+            BatteryManager batteryManager =
+                    (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
 
-            System.out.println(className);
-        };
+            pool.execute(() -> {
+                Consumer<Protos.ResultWrapper> consumer = resultWrapper -> {
+                    Protos.ResultWrapper.Result result = resultWrapper.getResults(0);
+                    ByteString byteString = result.getPayload();
+                    byte[] bytes = byteString.toByteArray();
+                    String className = new String(bytes);
 
-        Consumer<ErrorType> onDisconnect = errorType -> {
-            Log.e(TAG, "Disconnect Error:" + errorType.name());
+                    // System.out.println(className);
+                };
+
+                Consumer<ErrorType> onDisconnect = errorType -> {
+                    Log.e(TAG, "Disconnect Error:" + errorType.name());
+                    finish();
+                };
+
+                ServerComm serverComm = ServerComm.createServerComm(
+                        consumer, "vm030.elijah.cs.cmu.edu", 9099, getApplication(), onDisconnect);
+
+                File testImages = new File( Environment.getExternalStorageDirectory().getPath()
+                        + "/test_images/stirling");
+                int count = 0;
+                int total = 0;
+                long start = SystemClock.uptimeMillis();
+                long startEnergy = batteryManager.getLongProperty(
+                        BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream("/sdcard/output/stirling/baseline_" + System.currentTimeMillis() + ".txt");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                for (File classDir : testImages.listFiles()) {
+                    String correctClass = classDir.getName();
+                    for (File imageFile : classDir.listFiles()) {
+                        serverComm.send(packFrame(imageFile.getPath()), "images", true);
+
+                        if (count == 20) {
+                            long end = SystemClock.uptimeMillis();
+                            System.out.println("time change" + (end - start) + "ms");
+
+                            long endEnergy = batteryManager.getLongProperty(
+                                    BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+                            System.out.println("energy: " + endEnergy);
+                            System.out.println("energy change: " + (endEnergy - startEnergy) + "nWh");
+
+                            try {
+                                fos.write(("time change: " + (end - start) + "ms energy change: " + (startEnergy - endEnergy) + " nWh\n").getBytes());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            count = 0;
+                            start = SystemClock.uptimeMillis();
+
+                            startEnergy = batteryManager.getLongProperty(
+                                    BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+                        }
+
+                        if (total == 6000) {
+                            try {
+                                fos.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            running = false;
+                            return;
+                        }
+
+                        count++;
+                        total++;
+                    }
+                }
+            });
+
+            measureBattery(batteryManager);
             finish();
-        };
-
-        ServerComm serverComm = ServerComm.createServerComm(
-                consumer, "vm030.elijah.cs.cmu.edu", 9099, getApplication(), onDisconnect);
-
-
-        serverComm.send(packFrame("/sdcard/test_images/stirling/1screw/2_frame-0000.jpg"), "images", true);
+        });
     }
 
     public Protos.InputFrame packFrame(String path) {
@@ -74,5 +153,49 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void measureBattery(BatteryManager batteryManager) {
+        try {
+            FileOutputStream fos = new FileOutputStream(
+                    "/sdcard/output/bg_thread/thin_" +
+                            System.currentTimeMillis() + ".txt");
+            BatteryReceiver batteryReceiver = new BatteryReceiver();
+            IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            registerReceiver(batteryReceiver, intentFilter);
+
+            int endCount = 0;
+
+            while (true) {
+                long start = SystemClock.uptimeMillis();
+
+                long toWait = Math.max(0, ((start + 100) - SystemClock.uptimeMillis()));
+                Thread.sleep(toWait);
+
+                if (!running) {
+                    break;
+                }
+                int current = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW);
+                int voltage = batteryReceiver.getVoltage();
+                fos.write(("current: " + current + " voltage: " + voltage + "\n").getBytes());
+            }
+            fos.close();
+            unregisterReceiver(batteryReceiver);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class BatteryReceiver extends BroadcastReceiver {
+        private int voltage;
+
+        private int getVoltage() {
+            return voltage;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, Integer.MIN_VALUE);
+        }
     }
 }
