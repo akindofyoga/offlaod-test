7ade7876584cfcb32533adee6d5472e79920c6bf
Roger Iyengar <ri@rogeriyengar.com>
2022-11-01 23:45:58 -0400

    Object Detection on client

--- thin/app/src/main/java/com/example/thinclient/MainActivity.java
+++ thin/app/src/main/java/com/example/thinclient/MainActivity.java
@@ -9,6 +9,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Environment;
@@ -17,13 +20,24 @@ import android.util.Log;
 
 import com.google.protobuf.ByteString;
 
+import org.tensorflow.lite.gpu.CompatibilityList;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.task.core.BaseOptions;
+import org.tensorflow.lite.task.vision.detector.Detection;
+import org.tensorflow.lite.task.vision.detector.ObjectDetector;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
@@ -54,97 +68,160 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ExecutorService pool = Executors.newFixedThreadPool(2);
-        running = true;
         pool.execute(() -> {
             BatteryManager batteryManager =
                     (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
 
-            pool.execute(() -> {
-                Consumer<Protos.ResultWrapper> consumer = resultWrapper -> {
-                    Protos.ResultWrapper.Result result = resultWrapper.getResults(0);
-                    ByteString byteString = result.getPayload();
-                    byte[] bytes = byteString.toByteArray();
-                    String className = new String(bytes);
+            Consumer<Protos.ResultWrapper> consumer = resultWrapper -> {
+                Protos.ResultWrapper.Result result = resultWrapper.getResults(0);
+                ByteString byteString = result.getPayload();
+                byte[] bytes = byteString.toByteArray();
+                String className = new String(bytes);
 
-                    // System.out.println(className);
-                };
+                // System.out.println(className);
+            };
 
-                Consumer<ErrorType> onDisconnect = errorType -> {
-                    Log.e(TAG, "Disconnect Error:" + errorType.name());
-                    finish();
-                };
+            Consumer<ErrorType> onDisconnect = errorType -> {
+                Log.e(TAG, "Disconnect Error:" + errorType.name());
+                finish();
+            };
 
-                ServerComm serverComm = ServerComm.createServerComm(
-                        consumer, "vm030.elijah.cs.cmu.edu", 9099, getApplication(), onDisconnect);
+            ServerComm serverComm = ServerComm.createServerComm(
+                    consumer, "vm030.elijah.cs.cmu.edu", 9099, getApplication(), onDisconnect);
 
-                File testImages = new File( Environment.getExternalStorageDirectory().getPath()
-                        + "/test_images/stirling");
-                int count = 0;
-                int total = 0;
-                long start = SystemClock.uptimeMillis();
-                long startEnergy = batteryManager.getLongProperty(
-                        BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+            for (String odname : new String[] {/*"ed0.tflite",*/ "ed1.tflite", "ed2.tflite"}) {
+                running = true;
+                pool.execute(() -> {
+                    File testImages = new File(Environment.getExternalStorageDirectory().getPath()
+                            + "/test_images/stirling");
+                    int count = 0;
+                    int total = 0;
+                    long start = SystemClock.uptimeMillis();
+                    long startEnergy = batteryManager.getLongProperty(
+                            BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
 
-                FileOutputStream fos = null;
-                try {
-                    fos = new FileOutputStream("/sdcard/output/stirling/thin_" + System.currentTimeMillis() + ".txt");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                for (File classDir : testImages.listFiles()) {
-                    String correctClass = classDir.getName();
-                    for (File imageFile : classDir.listFiles()) {
-                        serverComm.send(packFrame(imageFile.getPath()), "images", true);
-
-                        if (count == 20) {
-                            long end = SystemClock.uptimeMillis();
-                            System.out.println("time change" + (end - start) + "ms");
-
-                            long endEnergy = batteryManager.getLongProperty(
-                                    BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
-                            System.out.println("energy: " + endEnergy);
-                            System.out.println("energy change: " + (endEnergy - startEnergy) + "nWh");
-
-                            try {
-                                fos.write(("time change: " + (end - start) + "ms energy change: " + (startEnergy - endEnergy) + " nWh\n").getBytes());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                            count = 0;
-                            start = SystemClock.uptimeMillis();
-
-                            startEnergy = batteryManager.getLongProperty(
-                                    BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
-                        }
-
-                        if (total == 6000) {
-                            try {
-                                fos.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            running = false;
-                            return;
-                        }
-
-                        count++;
-                        total++;
+                    FileOutputStream fos = null;
+                    try {
+                        //fos = new FileOutputStream("/sdcard/output/stirling/thin_" + System.currentTimeMillis() + ".txt");
+                        fos = new FileOutputStream("/sdcard/output/stirling/" + odname + "_" + System.currentTimeMillis() + ".txt");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
                     }
-                }
-            });
 
-            measureBattery(batteryManager);
+                    ObjectDetector objectDetector = null;
+                    try {
+                        ObjectDetector.ObjectDetectorOptions.Builder optionsBuilder =
+                                ObjectDetector.ObjectDetectorOptions.builder()
+                                        .setScoreThreshold(0.4f)
+                                        .setMaxResults(1);
+
+                        BaseOptions.Builder baseOptionsBuilder = BaseOptions.builder().setNumThreads(1);
+
+                        // This check is necessary because baseOptionsBuilder.useGpu(); will not work on Google
+                        // glass.
+                        //if ((new CompatibilityList()).isDelegateSupportedOnThisDevice()) {
+                            baseOptionsBuilder.useGpu();
+                        //}
+
+                        optionsBuilder.setBaseOptions(baseOptionsBuilder.build());
+
+                        File modelFile = new File("/sdcard/models/stirling/" + odname);
+                        objectDetector = ObjectDetector.createFromFileAndOptions(
+                                modelFile, optionsBuilder.build());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    for (File classDir : testImages.listFiles()) {
+                        String correctClass = classDir.getName();
+                        for (File imageFile : classDir.listFiles()) {
+//                            try {
+//                                FileInputStream fis = new FileInputStream(imageFile.getPath());
+//                                serverComm.send(packFrame(fis), "images", true);
+//                                fis.close();
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+
+                            Bitmap image = BitmapFactory.decodeFile(imageFile.getPath());
+                            ImageProcessor imageProcessor = (new ImageProcessor.Builder()).build();
+                            TensorImage tensorImage = imageProcessor.process(TensorImage.fromBitmap(image));
+
+                            List<Detection> detections = objectDetector.detect(tensorImage);
+                            if (detections.size() == 0) {
+                                continue;
+                            }
+                            if (detections.size() > 1) {
+                                throw new RuntimeException();
+                            }
+                            RectF rectF = detections.get(0).getBoundingBox();
+
+                            if ((rectF.top < 0) || (rectF.left < 0) || (rectF.width() == 0) ||
+                                    (rectF.height() == 0)) {
+                                continue;
+                            }
+
+                            if ((rectF.left + rectF.width()) > image.getWidth()) {
+                                System.out.println("bad");
+                                continue;
+                            }
+
+                            image = Bitmap.createBitmap(image, (int)rectF.left, (int)rectF.top,
+                                    (int)rectF.width(), (int)rectF.height());
+
+                            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+                                    byteArrayOutputStream.toByteArray());
+                            serverComm.send(packFrame(byteArrayInputStream), "images", true);
+
+                            if (count == 20) {
+                                long end = SystemClock.uptimeMillis();
+                                System.out.println("time change" + (end - start) + "ms");
+
+                                long endEnergy = batteryManager.getLongProperty(
+                                        BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+                                System.out.println("energy: " + endEnergy);
+                                System.out.println("energy change: " + (endEnergy - startEnergy) + "nWh");
+
+                                try {
+                                    fos.write(("time change: " + (end - start) + "ms energy change: " + (startEnergy - endEnergy) + " nWh\n").getBytes());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                count = 0;
+                                start = SystemClock.uptimeMillis();
+
+                                startEnergy = batteryManager.getLongProperty(
+                                        BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
+                            }
+
+                            if (total == 1000) {
+                                try {
+                                    fos.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                running = false;
+                                return;
+                            }
+
+                            count++;
+                            total++;
+                        }
+                    }
+                });
+
+                measureBattery(batteryManager, odname);
+            }
             finish();
         });
     }
 
-    public Protos.InputFrame packFrame(String path) {
+    public Protos.InputFrame packFrame(InputStream inputStream) {
         try {
-            FileInputStream fos = new FileInputStream(path);
-            ByteString imageContents = ByteString.readFrom(fos);
-            fos.close();
+            ByteString imageContents = ByteString.readFrom(inputStream);
 
             return Protos.InputFrame.newBuilder()
                     .setPayloadType(Protos.PayloadType.IMAGE)
@@ -155,16 +232,14 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
-    private void measureBattery(BatteryManager batteryManager) {
+    private void measureBattery(BatteryManager batteryManager, String odname) {
         try {
             FileOutputStream fos = new FileOutputStream(
-                    "/sdcard/output/bg_thread/thin_" +
+                    "/sdcard/output/bg_thread/" + odname + "_" +
                             System.currentTimeMillis() + ".txt");
             BatteryReceiver batteryReceiver = new BatteryReceiver();
             IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
             registerReceiver(batteryReceiver, intentFilter);
-
-            int endCount = 0;
 
             while (true) {
                 long start = SystemClock.uptimeMillis();
